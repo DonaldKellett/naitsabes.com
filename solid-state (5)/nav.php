cd3bf6a9bb590ffc487a84882504aefa2e6ed964
DonaldKellett <i.donaldl@me.com>
2016-07-05 22:14:56 +0800

    Add latest website design and content

---
+++ solid-state (5)/nav.php
@@ -0,0 +1,12 @@
+<nav id="menu">
+  <div class="inner">
+    <h2>Menu</h2>
+    <ul class="links">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="code.php">Featured Coding Projects</a></li>
+      <li><a href="http://forums.naitsabes.com/" target="_blank" style="font-weight: bold;">Forums</a></li>
+      <li><a href="http://developer.naitsabes.com/" target="_blank" style="font-weight: bold;">Web Development Services</a></li>
+    </ul>
+    <a href="#" class="close">Close</a>
+  </div>
+</nav>
