339bf2bf310b5b8d854e84ee7539c1c1e8412e94
Donald Sebastian Leung <i.donaldl@me.com>
2018-06-04 22:38:36 +0800

    Add new website design using Dimension HTML5UP template

---
+++ assets/php/contact.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * contact.php
+ * A script powering the contact form located at /index.php
+ * (c) Donald Sebastian Leung.  All rights reserved.
+ */
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require "../libs/PHPMailer-6.0.5/src/PHPMailer.php";
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST" || !isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]))
+  echo "<span style=\"color: red\"><span class=\"icon fa-exclamation-circle\"></span> The message was not sent since the form was not submitted properly or is missing required data fields.</span>";
+elseif (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+  echo "<span style=\"color: red\"><span class=\"icon fa-exclamation-circle\"></span> The message was not sent because:<br /><ul>";
+  if (empty($_POST["name"]))
+    echo "<li>The &quot;name&quot; field was empty</li>";
+  if (empty($_POST["email"]))
+    echo "<li>The &quot;email&quot; field was empty</li>";
+  elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
+    echo "<li>An invalid email address was provided</li>";
+  if (empty($_POST["message"]))
+    echo "<li>The &quot;message&quot; field was empty</li>";
+  echo "</ul></span>";
+} else {
+  $mail = new PHPMailer;
+  $mail->setFrom($_POST["email"], $_POST["name"]);
+  $mail->addReplyTo($_POST["email"], $_POST["name"]);
+  $mail->addAddress("i.donaldl@me.com");
+  $mail->Subject = "Private Message from {$_POST["name"]}";
+  $mail->Body = "{$_POST["message"]}\n\n- Sent from http://naitsabes.com";
+  echo $mail->send() ?
+    "<span style=\"color: green\"><span class=\"icon fa-check-circle\"></span> The message was sent successfully.</span>" :
+    "<span style=\"color: red\"><span class=\"icon fa-exclamation-circle\"></span> The message could not be sent due to an unknown error.  If this persists, please consider sending an email directly to <a href=\"mailto:i.donaldl@me.com\">i.donaldl@me.com</a>.  Don't forget to notify me of this error as well ;)</span>";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,401 @@
+<!DOCTYPE HTML>
+<!--
+	Dimension by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Donald S. Leung - A Computer Science and Engineering student at HKUST</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/libs/fontawesome-free-5.0.13/web-fonts-with-css/css/fontawesome-all.css">
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="fas fa-microchip fa-lg"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>Donald S. Leung</h1>
+								<p>A <a href="https://www.cse.ust.hk" target="_blank">Computer Science and Engineering</a> student at <a href="https://www.ust.hk" target="_blank">HKUST</a></p>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="#about">About Me</a></li>
+								<li><a href="#contact">Contact</a></li>
+								<!-- <li><a href="#elements">Elements</a></li> -->
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Intro -->
+							<article id="intro">
+								<h2 class="major">Intro</h2>
+								<span class="image main"><img src="images/pic01.jpg" alt="" /></span>
+								<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+							</article>
+
+						<!-- Work -->
+							<article id="work">
+								<h2 class="major">Work</h2>
+								<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+							</article>
+
+						<!-- About -->
+							<article id="about">
+								<h2 class="major">About Me</h2>
+								<p>
+									<a style="border-bottom: medium none" href="https://codewars.com/users/donaldsebleung" target="_blank">
+										<img src="https://www.codewars.com/users/donaldsebleung/badges/micro" style="width: 150px" alt="donaldsebleung on Codewars" />
+									</a>
+								</p>
+								<p>I am currently a Year 2 undergraduate student in <a href="https://www.ust.hk/" target="_blank">The Hong Kong University of Science and Technology</a> under the <a href="https://www.cse.ust.hk/" target="_blank">Department of Computer Science and Engineering</a>.</p>
+								<p>
+									I was deeply fascinated with computers from a young age, mainly at their ability to perform everyday tasks such as sending a message or calculating the result of a mathematical expression in the blink of an eye.
+									This motivated me to try to learn programming at a young age - I bought an introductory book called <a href="http://ineasysteps.com/products-page/all_books/c-programming-in-easy-steps-4th-edition/" target="_blank">"C++ Programming in Easy Steps (4th Edition)" by Mike McGrath</a> when I was about 12.
+									Unfortunately I was not able to make any progress back then because I couldn't even get the <span title="A program that allows the computer to understand C++">compiler</span> properly installed on my Windows desktop.
+									Nevertheless, compulsory IT courses offered in <a href="http://www.css.edu.hk" target="_blank">CSS</a> during the first two years of my secondary education allowed me to learn simple programming-related concepts through the visual <span title="Ermmm ... this is debatable ... ;)">programming language</span> <a href="https://scratch.mit.edu" target="_blank">Scratch</a>.
+								</p>
+								<p>
+									My major breakthrough occurred when I joined an extra-curricular activity called "Web and App Development" provided by <a href="https://hk.bsdacademy.com" target="_blank">BSD Academy</a> in <a href="https://www.kellettschool.com" target="_blank">Kellett School</a> when I was in Year 10.
+									Through that 12-hour course (2 hours per session spread through 6 weeks), I learned how to design simple websites using the core Web technologies, namely HTML, CSS and <span title="Client-side JS, i.e. JS code that runs directly in your browser">JS</span>.
+									This rekindled my interest in computer programming and I spent a lot of my spare time designing simple web pages and widgets as a result.
+								</p>
+								<p>
+									After freestyling for about a year in writing simple webpages and games using HTML+CSS+JS, I stumbled upon <a href="http://www.codewars.com/r/2QXbZQ" target="_blank">Codewars</a> which is a site for programmers/developers of all types and abilities to train on their programming skills and become a better programmer.
+									I was immediately in love with it and became addicted to solving challenge upon challenge on that website.
+									As a direct result, my programming and problem-solving skills skyrocketed to what it is today.
+									My focus has also gradually shifted from web design to more generic problem-solving through writing programs and designing simple algorithms; however, I still specialize in Web Development - if you want a simple personal/commercial website designed, feel free to <a href="#contact">contact me</a> through my contact form ;)
+								</p>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact">
+								<h2 class="major">Contact</h2>
+								<form method="post" action="#">
+									<div class="field half first">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input type="email" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="4"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" id="submit" value="Send Message" class="special" /></li>
+										<li><input type="reset" value="Reset" /></li>
+									</ul>
+								</form>
+								<p id="status">
+									<!-- Status of Email Request upon clicking "Send Message" -->
+								</p>
+								<ul class="icons">
+									<li><a href="https://www.facebook.com/manbehindthescreen01" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+									<li><a href="https://github.com/DonaldKellett" class="icon fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+									<li><a href="https://codewars.com/users/donaldsebleung" class="icon fa-code" target="_blank"><span class="label">Codewars</span></a></li>
+								</ul>
+							</article>
+
+						<!-- Elements -->
+							<article id="elements">
+								<h2 class="major">Elements</h2>
+
+								<section>
+									<h3 class="major">Text</h3>
+									<p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.</p>
+									<hr />
+									<h2>Heading Level 2</h2>
+									<h3>Heading Level 3</h3>
+									<h4>Heading Level 4</h4>
+									<h5>Heading Level 5</h5>
+									<h6>Heading Level 6</h6>
+									<hr />
+									<h4>Blockquote</h4>
+									<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan faucibus. Vestibulum ante ipsum primis in faucibus lorem ipsum dolor sit amet nullam adipiscing eu felis.</blockquote>
+									<h4>Preformatted</h4>
+									<pre><code>i = 0;
+
+while (!deck.isInOrder()) {
+    print 'Iteration ' + i;
+    deck.shuffle();
+    i++;
+}
+
+print 'It took ' + i + ' iterations to sort the deck.';</code></pre>
+								</section>
+
+								<section>
+									<h3 class="major">Lists</h3>
+
+									<h4>Unordered</h4>
+									<ul>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Alternate</h4>
+									<ul class="alt">
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Ordered</h4>
+									<ol>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis viverra.</li>
+										<li>Felis enim feugiat.</li>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis lorem.</li>
+										<li>Felis enim et feugiat.</li>
+									</ol>
+									<h4>Icons</h4>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+									</ul>
+
+									<h4>Actions</h4>
+									<ul class="actions">
+										<li><a href="#" class="button special">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions vertical">
+										<li><a href="#" class="button special">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Table</h3>
+									<h4>Default</h4>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+
+									<h4>Alternate</h4>
+									<div class="table-wrapper">
+										<table class="alt">
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+								</section>
+
+								<section>
+									<h3 class="major">Buttons</h3>
+									<ul class="actions">
+										<li><a href="#" class="button special">Special</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button">Default</a></li>
+										<li><a href="#" class="button small">Small</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button special icon fa-download">Icon</a></li>
+										<li><a href="#" class="button icon fa-download">Icon</a></li>
+									</ul>
+									<ul class="actions">
+										<li><span class="button special disabled">Disabled</span></li>
+										<li><span class="button disabled">Disabled</span></li>
+									</ul>
+								</section>
+
+								<section>
+									<h3 class="major">Form</h3>
+									<form method="post" action="#">
+										<div class="field half first">
+											<label for="demo-name">Name</label>
+											<input type="text" name="demo-name" id="demo-name" value="" placeholder="Jane Doe" />
+										</div>
+										<div class="field half">
+											<label for="demo-email">Email</label>
+											<input type="email" name="demo-email" id="demo-email" value="" placeholder="jane@untitled.tld" />
+										</div>
+										<div class="field">
+											<label for="demo-category">Category</label>
+											<div class="select-wrapper">
+												<select name="demo-category" id="demo-category">
+													<option value="">-</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+											</div>
+										</div>
+										<div class="field half first">
+											<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+											<label for="demo-priority-low">Low</label>
+										</div>
+										<div class="field half">
+											<input type="radio" id="demo-priority-high" name="demo-priority">
+											<label for="demo-priority-high">High</label>
+										</div>
+										<div class="field half first">
+											<input type="checkbox" id="demo-copy" name="demo-copy">
+											<label for="demo-copy">Email me a copy</label>
+										</div>
+										<div class="field half">
+											<input type="checkbox" id="demo-human" name="demo-human" checked>
+											<label for="demo-human">Not a robot</label>
+										</div>
+										<div class="field">
+											<label for="demo-message">Message</label>
+											<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="special" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</section>
+
+							</article>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">&copy; <?php echo date('Y'); ?> Donald Sebastian Leung. All rights reserved. Design: <a href="https://html5up.net" target="_blank">HTML5 UP</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script type="text/javascript">
+				document.getElementById("submit").onclick = function (event) {
+					event.preventDefault();
+					var xhttp = new XMLHttpRequest();
+					xhttp.onreadystatechange = function () {
+						if (this.readyState === 4 && this.status === 200)
+							document.getElementById("status").innerHTML = this.responseText;
+						window.setTimeout(function () {
+							document.getElementById("status").innerHTML = "";
+						}, 5000);
+					};
+					xhttp.open("POST", "assets/php/contact.php", true);
+					xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+					xhttp.send(
+						"name=" + encodeURIComponent(document.getElementById("name").value) + "&" +
+						"email=" + encodeURIComponent(document.getElementById("email").value) + "&" +
+						"message=" + encodeURIComponent(document.getElementById("message").value)
+					);
+				};
+			</script>
+
+	</body>
+</html>
